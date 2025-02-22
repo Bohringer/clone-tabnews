0cb9068ea738b7fee98b4e20f38273b45e7f7132
Luiz Bohringer <67234381+Bohringer@users.noreply.github.com>
2025-02-22 19:30:24 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,5 @@
 function Home() {
-    return (
-        <div>
-            <h1>
-                Bibi, preste muita atenção!!!<br></br>
-                Eu ia colocar um gif de gato, mas mó rolê kkkkkkkk<br></br>    
-                Queria que soubesse que sempre vou me preocupar contigo<br></br>    
-                E nos seus piores dias pode me chamar que estarei aqui.<br></br>    
-                <br /><br />
-                Bianca, teus olhos são feitos de infinito,<br></br>    
-                Um universo azul, sereno e bonito.<br></br>    
-                Neles me perco, sem medo ou razão,<br></br>    
-                Como um poeta que segue o coração.<br></br>    
-                <br /><br />
-                É chatinha, eu sei,<br></br>    
-                Mas de um jeito que só me faz querer te ver outras vezes (quase sempre hihi).<br></br>  
-                Teu brilho reluz na imensidão,<br></br>    
-                Estrela mais linda desta constelação.<br></br>    
-                <br /><br />
-                Te amo❤️❤️
-            </h1>
-        </div>
-    );
+  return <h1>Em construção</h1>;
 }
 
-export default function IndexPage() {
-    return (
-        <div>
-            <Home />
-            <h1>Menti coloquei mesmo assim hihihi</h1>
-            <img 
-                src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZmM5Mm95cjB1N3Y5ZmQ0d3ZubXhsaXBlNXN1dm9oZm14bms0ZXg3aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MDJ9IbxxvDUQM/giphy.gif" 
-                alt="GIF animado" 
-            />
-        </div>
-    );
-}
+export default Home;
